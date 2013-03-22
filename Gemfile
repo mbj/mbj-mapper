501b7dc442557df3f3231153bfa5e0cd560eed6b
Markus Schirp <mbj@schirp-dso.com>
2013-03-22 21:44:52 +0100

    Cleanup spike with results from discussion with snusnu
    http://irclogger.com/.datamapper/2013-03-22#1363979144

--- Gemfile
+++ Gemfile
@@ -3,10 +3,11 @@ source 'https://rubygems.org'
 gem 'veritas',        :git => 'https://github.com/dkubb/veritas.git'
 gem 'veritas-fuzzer', :git => 'https://github.com/mbj/veritas-fuzzer.git'
 gem 'veritas-sexp',   :git => 'https://github.com/mbj/veritas-sexp.git'
-gem 'composition',    :git => 'https://github.com/mbj/composition.git'
+gem 'dm-session',     :path => '../dm-session' # :git => 'https://github.com/mbj/dm-session.git'
 
 gem 'virtus'
 gem 'abstract_type'
+gem 'concord'
 
 gem 'devtools',    :git => 'https://github.com/datamapper/devtools.git'
 eval File.read('Gemfile.devtools')
--- spike.rb
+++ spike.rb
@@ -1,23 +1,154 @@
 #encoding: utf-8
 $: << 'lib'
-require 'mapper'
+require 'dm-session'
+require 'virtus'
+require 'concord'
+require 'veritas'
+require 'veritas-sexp'
+require 'pp'
 
 class Person
-  include Virtus
+  include Virtus::ValueObject
 
   attribute :id, Integer
   attribute :firstname, String
   attribute :lastname, String
 end
 
+module DataMapper
 
-header = Veritas::Relation::Header.coerce([[:id, Integer], [:firstname, String], [:lastname, String]], :keys => [:id])
+  class Transformer
+    # Minimal 1:1 virtus <=> tuple mapping
+    class Virtus
+      include Concord.new(:model, :header)
 
-transformer = Mapper::Transformer::Virtus.new(header, Person)
+      def load(tuple)
+        loader(tuple).object
+      end
 
-relation = Veritas::Relation.new(header, [[ 1, 'Markus', 'Schirp' ], [ 2, 'Dan', 'Kubb' ], [ 3, 'Piotr', 'Solnica' ], [ 4, 'Martin', 'Gamsjäger' ]])
+      def loader(tuple)
+        Loader.new(model, header, tuple)
+      end
 
-Mapper::Reader.new(relation, transformer).each do |person|
-  p person
+      def dumper(object)
+        Dumper.new(model, header, object)
+      end
+
+      def dump(object)
+        dumper(object).tuple
+      end
+
+      class Loader
+        include Adamantium::Flat, Concord.new(:model, :header, :tuple)
+
+        # Add itentity here for dm-session support
+
+        def object
+          model.new(tuple.header.each_with_object({}) { |attribute, document| document[attribute.name] = tuple.call(attribute) })
+        end
+        memoize :object, :freezer => :noop
+
+      end
+
+      class Dumper
+        include Adamantium::Flat, Concord.new(:model, :header, :object)
+
+        # Add itentity here for dm-session support
+
+        def tuple
+          object
+        end
+        memoize :tuple, :freezer => :noop
+      end
+    end
+  end
+
+  class Mapper
+    include Concord.new(:relation, :transformer)
+
+    def all(relation = self.relation)
+      relation = yield relation if block_given?
+      relation.map do |tuple|
+        load(tuple)
+      end
+    end
+
+    def one(relation = self.relation)
+      relation = yield relation if block_given?
+      tuples = sort(relation).take(2).to_a
+      case tuples.length
+      when 2
+        raise "Read than one tuple"
+      when 1
+        load(tuples.first)
+      else
+        nil
+      end
+    end
+
+  private
+
+    def load(tuple)
+      transformer.load(tuple)
+    end
+
+    # Sort relation if not sorted already ;)
+    #
+    # This is lame see my comment about Relation#sorted? predicate in dkubb/veritas.
+    #
+    # https://github.com/mbj/veritas/commit/ef70583330f4743d4a58374ef7d2077a7d996fc7#commitcomment-2863926
+    #
+    def sort(relation)
+      if relation.kind_of?(Veritas::Relation::Operation::Order)
+        return relation
+      end
+      # There must be a shortcut for this ;)
+      relation.sort_by { relation.header.map(&:asc) }
+    end
+
+  end
+
+  class Environment
+    include Adamantium::Flat, Concord.new(:mappers)
+
+    def mapper(model)
+      mappers.fetch(model)
+    end
+
+  end
 end
 
+header = Veritas::Relation::Header.coerce([[:id, Integer], [:firstname, String], [:lastname, String]])
+
+# Basic 1:1 virtus transformer 
+transformer = DataMapper::Transformer::Virtus.new(Person, header)
+
+tuples = [
+  [ 1, 'Markus', 'Schirp'    ], 
+  [ 2, 'Dan',    'Kubb'      ], 
+  [ 3, 'Piotr',  'Solnica'   ], 
+  [ 4, 'Martin', 'Gamsjäger' ]
+]
+
+relation = Veritas::Relation.new(header, tuples).sort_by { |r| [r.id.asc, r.firstname.asc, r.lastname.asc] }
+
+#pp Veritas::Sexp::Generator.visit(relation)
+
+mappers = {
+  Person => DataMapper::Mapper.new(relation, transformer)
+}.freeze
+
+env = DataMapper::Environment.new(mappers)
+
+markus = env.mapper(Person).one { |relation| relation.restrict(:firstname => 'Markus') }
+p markus # => Person instance
+
+john = env.mapper(Person).one { |relation| relation.restrict(:firstname => 'John') }
+p john # => nil
+
+p env.mapper(Person).all # => Enumerable<Person>
+
+# Pass an explicit restriction, might be from crazy joining some stuff....
+p env.mapper(Person).all(relation) # => Enumerable<Person>
+
+# More fun ahead with dm-session integration
